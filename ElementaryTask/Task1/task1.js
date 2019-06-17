f21826933c8e1842db89eeebd19f3532d04b8789
Iryna Dauchyonas <irynadauchyonas@MacBook-Air-Iryna.local>
2019-06-17 20:51:04 +0300

    update Elementary Task 1

--- ElementaryTask/Task1/task1.js
+++ ElementaryTask/Task1/task1.js
@@ -1,25 +1,53 @@
 function chessBoard(width, height, char) {
-  let result = '\n';
+  let result = 0;
 
-  if(arguments.length !== 3 ||
-      !Number.isInteger(width) || width === 0|| !Number.isInteger(height) || height === 0 ||
-      typeof char !== 'string' || char.length !== 1 ) {
-        result = {status: 'faild', 
-          reason: 'Three arguments are expected: integer, integer, string with length = 1'};
-      } else {
-          for(let i = 0; i < height; i++) {
-              if (i % 2 === 0) {
-                for(let i = 0; i < width; i++) {
-                  (i % 2 === 0) ? result += char : result += ' ';
-                }
-              } else {
-                  for(let i = 0; i < width; i++) {
-                    (i % 2 === 0) ? result += ' ' : result += char;
-                  }
+  if(arguments.length === 3) {
+    result = checkArguments(width, height, char);
+    if (result === 0) {
+      result = '\n';
+
+      for(let i = 0; i < height; i++) {
+          if (i % 2 === 0) {
+            for(let i = 0; i < width; i++) {
+              (i % 2 === 0) ? result += char : result += ' ';
+            }
+          } else {
+              for(let i = 0; i < width; i++) {
+                (i % 2 === 0) ? result += ' ' : result += char;
               }
-              result += '\n';
-          }
+          } 
+
+          result += '\n';
       }
+    }
+  } else {
+    result = {
+      status: 'faild', 
+      reason: 'Three arguments are expected: integer, integer, string with length = 1'
+    };
+  }
+
+  return result;
+}
+
+function checkArguments(width, height, char) {
+  result = 0;
+
+  if (!Number.isInteger(width) || width === 0 ||
+      !Number.isInteger(height) || height === 0) {
+        result = {
+          status: 'faild', 
+          reason: 'First and second arguments expected to be integer and mor than 0'
+        };
+  }
+
+  if (typeof char !== 'string' || char.length !== 1) {
+    result = {
+      status: 'faild', 
+      reason: 'Third argument expected to be string with length = 1'
+    };
+  } 
+
   return result;
 }
 
--- ElementaryTask/Task1/test.js
+++ ElementaryTask/Task1/test.js
@@ -1,8 +1,19 @@
 describe("chessBoard", function() {
-  it("Выводит сообщение о некорректных входных данных", function() {
-    assert.deepEqual(chessBoard(5.5, 10, '*'), {status: 'faild', 
+  it("Вызов без аргументов ожидает соответствующее сообщение", function() {
+    assert.deepEqual(chessBoard(), {status: 'faild', 
     reason: 'Three arguments are expected: integer, integer, string with length = 1'});
   });
+
+  it("Первый или второй аргумент не является целым числом, выводит соответствующее сообщение", function() {
+    assert.deepEqual(chessBoard(5.5, 10, '#'), {status: 'faild', 
+    reason: 'First and second arguments expected to be integer and mor than 0'});
+  });
+
+  it("Третий аргумент не является строкой с длиной = 1, выводит соответствующее сообщение", function() {
+    assert.deepEqual(chessBoard(10, 10, '#№№'), {status: 'faild', 
+    reason: 'Third argument expected to be string with length = 1'});
+  });
+
   it("Печатает шахматную доску 6 на 4 символом @ ", function() {
     assert.equal(chessBoard(6, 4, '@'),
              '\n@ @ @ \n @ @ @\n@ @ @ \n @ @ @\n');
